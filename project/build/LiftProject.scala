a9f62fbb72f5586c3a9cae7df8c61a91c9a0b574
Tommy C. Li <tommycli@ucla.edu>
2012-04-03 19:04:58 -0600

    Make build correctly again

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -2,7 +2,7 @@ import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) 
   with com.untyped.ClosureCompilerPlugin {
-  val liftVersion = "2.2"
+  val liftVersion = "2.4"
 
   // uncomment the following if you want to use the snapshot repo
   // val scalatoolsSnapshot = ScalaToolsSnapshots
@@ -16,6 +16,6 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info)
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
     "junit" % "junit" % "4.5" % "test->default",
     "ch.qos.logback" % "logback-classic" % "0.9.26",
-    "org.scala-tools.testing" %% "specs" % "1.6.6" % "test->default"
+    "org.scala-tools.testing" %% "specs" % "1.6.9" % "test->default"
   ) ++ super.libraryDependencies
 }
