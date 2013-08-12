86ce7d24ff0f7f4b8fffada3498e368d542da39c
Tommy C. Li <tommycli@keepword.com>
2013-08-11 20:30:10 -0700

    update dependencies

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -13,11 +13,10 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
   lazy val JavaNet = "Java.net Maven2 Repository" at "http://download.java.net/maven/2/"
 
   override def libraryDependencies = Set(
-    "net.liftweb" %% "lift-webkit" % liftVersion.value.toString % "compile",
-    "net.liftweb" %% "lift-mapper" % liftVersion.value.toString % "compile",
-    "org.mortbay.jetty" % "jetty" % "6.1.26" % "test",
-    "junit" % "junit" % "4.7" % "test",
+    "net.liftweb" %% "lift-webkit" % liftVersion.value.toString % "compile->default",
+    "org.mortbay.jetty" % "jetty" % "6.1.26" % "test->default",
+    "junit" % "junit" % "4.7" % "test->default",
     "ch.qos.logback" % "logback-classic" % "0.9.26",
-    "org.scala-tools.testing" %% "specs" % "1.6.8" % "test"
+    "org.scala-tools.testing" %% "specs" % "1.6.9" % "test->default"
   ) ++ super.libraryDependencies
 }
